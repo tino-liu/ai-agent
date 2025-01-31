714dbcdd2d0c422cf0e09f41901633357cb3a258
Tino Liu [SSW] <tinoliu@ssw.com.au>
2025-01-31 21:12:09 +0800

    added navigation provider and global state for nav

---
+++ lib/NavigationProvider.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { createContext, useState } from "react";
+
+interface NavigationContextType {
+    isMobileNavOpen: boolean;
+    setIsMobileNavOpen: (isOpen: boolean) => void;
+    closeMobileNav: () => void;
+}
+
+export const NavigationContext = createContext<NavigationContextType>({
+    isMobileNavOpen: false,
+    setIsMobileNavOpen: () => { },
+    closeMobileNav: () => { },
+});
+
+export default function NavigationProvider({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+
+    const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
+    const closeMobileNav = () => setIsMobileNavOpen(false);
+
+    return (
+        <NavigationContext value={{ isMobileNavOpen, setIsMobileNavOpen, closeMobileNav }}>
+            {children}
+        </NavigationContext>
+    );
+}
