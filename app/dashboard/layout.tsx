caf89fe49fbac1d1350da3568ac0353bc6f65fdb
Tino Liu [SSW] <tinoliu@ssw.com.au>
2025-01-31 17:52:01 +0800

    layout of dashboard

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,27 @@
+"use client";
+
+import Header from "@/components/Header";
+import { Authenticated } from "convex/react";
+
 export default function DashboardLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
-    return <div>{children}</div>
+    return (
+        <div className="flex h-screen">
+            <Authenticated>
+                {/* <Sidebar /> */}
+                <h1>Sidebar</h1>
+            </Authenticated>
+
+            <div className="flex-1">
+                <Header />
+                <main>
+                    {children}
+                </main>
+            </div>
+
+        </div>
+    );
 }
