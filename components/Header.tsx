976ccf66829e5633c64b46e2084c660b3cb2632f
Tino Liu [SSW] <tinoliu@ssw.com.au>
2025-02-01 12:03:28 +0800

    use global state from navigation context inside header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,14 @@
 import { UserButton } from '@clerk/nextjs'
-import React from 'react'
+import React, { use } from 'react'
 import { Button } from './ui/button'
 import { HamburgerMenuIcon } from '@radix-ui/react-icons'
+import { NavigationContext } from '@/lib/NavigationProvider'
 
 
 function Header() {
+
+    const { setIsMobileNavOpen, isMobileNavOpen } = use(NavigationContext);
+
     return (
         <header className="border-b border-gray-200/50 bg-white/80 backdrop-blur-xl sticky top-0 z-50">
             <div className="flex items-center justify-between px-4 py-3">
@@ -12,7 +16,7 @@ function Header() {
                     <Button
                         variant="ghost"
                         size="icon"
-                        // onClick={() => setIsMobileNavOpen(true)}
+                        onClick={() => setIsMobileNavOpen(true)}
                         className="md:hidden text-gray-500 hover:text-gray-700 hover:bg-gray-100/50"
                     >
                         <HamburgerMenuIcon className="h-5 w-5" />
