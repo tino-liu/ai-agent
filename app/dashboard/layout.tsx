0bc584ce6c47e2355efc54cbcf416c4b48cf0978
Tino Liu [SSW] <tinoliu@ssw.com.au>
2025-02-01 12:02:27 +0800

    wrap dashboard within navigation provider

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Header from "@/components/Header";
+import NavigationProvider from "@/lib/NavigationProvider";
 import { Authenticated } from "convex/react";
 
 export default function DashboardLayout({
@@ -9,19 +10,21 @@ export default function DashboardLayout({
     children: React.ReactNode;
 }) {
     return (
-        <div className="flex h-screen">
-            <Authenticated>
-                {/* <Sidebar /> */}
-                <h1>Sidebar</h1>
-            </Authenticated>
+        <NavigationProvider>
+            <div className="flex h-screen">
+                <Authenticated>
+                    {/* <Sidebar /> */}
+                    <h1>Sidebar</h1>
+                </Authenticated>
 
-            <div className="flex-1">
-                <Header />
-                <main>
-                    {children}
-                </main>
+                <div className="flex-1">
+                    <Header />
+                    <main>
+                        {children}
+                    </main>
+                </div>
+
             </div>
-
-        </div>
+        </NavigationProvider>
     );
 }
